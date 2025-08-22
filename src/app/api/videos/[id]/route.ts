f62696d7cda82e8becf931dad4b6d0359c366b84
RM Muzammil <ramuzamil44@gmail.com>
2025-08-22 15:12:22 +0500

    delete route update

--- src/app/api/videos/[id]/route.ts
+++ src/app/api/videos/[id]/route.ts
@@ -1,18 +1,21 @@
 // app/api/videos/[id]/route.ts
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 // import dbConnect from "@/lib/dbConnect"; // your MongoDB connection utility
 import Video from "@/models/Video";
 import { connect } from "@/lib/db";
+interface Params {
+    params: {
+        id: string;
+    };
+}
 
 export async function GET(
-    req: Request,
-    context: { params: { id: string } }
+    req: NextRequest, { params }: Params
 ) {
     try {
         await connect()
-        const { id } = context.params;
 
-        const video = await Video.findById(id).populate("owner", "name email image")
+        const video = await Video.findById(params.id).populate("owner", "name email image")
 
         if (!video) {
             return NextResponse.json({ error: "Video not found" }, { status: 404 });
